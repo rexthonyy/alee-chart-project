63f629ebd1d7d75fc03fdd838db1f1b87fbcb523
rexthonyy <rexthonyy@gmail.com>
2021-04-21 19:00:53 +0100

    Google sheet implementation

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-//const mongoose = require('mongoose');
 const MonkeyLearn = require('monkeylearn')
+const GoogleSpreadsheet = require('google-spreadsheet');
+const { promisify } = require('util');
+const creds = require('./client_secret.json');
 
 const app = express();
 
@@ -10,29 +12,41 @@ app.use(express.static('public'));
 const ml = new MonkeyLearn('b1ee507b93b9f75b0d9658bec3f0d42f2344a68c');
 let model_id = 'cl_pi3C7JiL';
 
-//database connection
-const DB_URL = "mongodb://127.0.0.1:27017/alee-chart";
-
-// const MONGODB_URI = process.env.MONGODB_URI || DB_URL;
-// mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
-// const db = mongoose.connection;
-// db.on('error', (error) => console.error(error));
-// db.once('open', () => console.log('Connected to mongodb Database'));
-
 const LOCAL_PORT = 3000;
 const PORT = process.env.PORT || LOCAL_PORT;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-// Input : text (string)
-// Process : sentiment analysis
-// Output : score (0 - 4) (number)
 app.post("/", async (req, res) => {
-	let data = [req.body.inputText];
-	ml.classifiers.classify(model_id, data).then(result => {
-		let score = getScore(result.body[0].classifications[0]);
-		res.json({status: 'success', score: score, text: result.body[0].text});
-	});
+	let docId = req.body.docId;
+	let sheetIndex = req.body.sheetIndex;
+	
+	accessSpreadsheet(res, docId, sheetIndex);
 });
+
+async function accessSpreadsheet(res, docId, sheetIndex) {
+	const doc = new GoogleSpreadsheet(docId);
+	await promisify(doc.useServiceAccountAuth)(creds);
+	const info = await promisify(doc.getInfo)();
+	const sheet = info.worksheets[sheetIndex];
+	const rows = await promisify(sheet.getRows)({
+		offset: 1
+	});
+	
+	let sentiments = [];
+	rows.forEach(row => {
+		let data = [row.message];
+		ml.classifiers.classify(model_id, data).then(result => {
+			let score = getScore(result.body[0].classifications[0]);
+			sentiments.push({
+				message: result.body[0].text,
+				score: score
+			});
+			if(sentiments.length == rows.length){
+				res.json({status: 'success', result: sentiments});
+			}
+		});
+	});
+}
 
 function getScore(result){
 	let tag_name = result.tag_name;
